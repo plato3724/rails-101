3e77a3a31d403b00fdcf4c828f625404058e6097
yhba <plato3724@gmail.com>
2016-12-11 22:02:01 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
 	before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-				
+	before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]			
 	def index
 		@groups = Group.all
 	end	
@@ -13,10 +13,6 @@ class GroupsController < ApplicationController
 	end
 
 	def edit
-	  @group = Group.find(params[:id])
-	  if current_user != @group.user
-	  	redirect_to root_path, alert: "You have no permission."
-	  end  	
 	end   	
 
 	
@@ -32,12 +28,6 @@ class GroupsController < ApplicationController
 	end
 
 	def update
-		@group =Group.find(params[:id])
-
-		if current_user != @group.user
-		   redirect_to root_path, aler: "You have no permission."
-		end   	
-
 		if @group.update(group_params)
 
 			redirect_to groups_path,notice: "Update Success"
@@ -47,12 +37,6 @@ class GroupsController < ApplicationController
 	end
 	
 	def destroy
-	  @group = Group.find(params[:id])
-
-	  if current_user != @group.user
-	  	redirect_to root_path, alert: "You have no permission"
-	  end
-	  	
 	  @group.destroy
 	  flash[:alert] = "Group deleted"
 	  redirect_to groups_path
@@ -60,6 +44,15 @@ class GroupsController < ApplicationController
 
 
 	private
+
+	def find_group_and_check_permission
+		@group = Group.find(params[:id])
+
+		if current_user != @group.user
+		   redirect_to root_path, alter: "You have no permission."
+		end 
+	end 	
+
 
 	def group_params
 	  params.require(:group).permit(:title, :description)
