cf4dcb63190e878a01317a56e2a66907266ec3ac
yhba <plato3724@gmail.com>
2016-12-11 21:09:59 +0800

    user_id to group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,16 @@
 class GroupsController < ApplicationController
 	before_action :authenticate_user! , only: [:new]
+	def create 
+		@group = Group.new(group_params)
+		@group.user = current_user
 
+		if @group.save
+			redirect_to groups_path
+		else 
+			render :new
+		end
+	end
+				
 	def index
 		@groups = Group.all
 	end	
